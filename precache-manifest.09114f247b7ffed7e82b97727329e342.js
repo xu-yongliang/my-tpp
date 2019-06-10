34d6b7621d46eeb7d982964fab9cd50f7c407254
xuyongliang <xuyongliang@xiaomi.com>
2019-06-10 14:02:04 +0800

    Updates

---
+++ precache-manifest.09114f247b7ffed7e82b97727329e342.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc129ceb354cae253af6d3d548028a6",
+    "url": "/my-tpp/index.html"
+  },
+  {
+    "revision": "865b23f103a429672f40",
+    "url": "/my-tpp/static/css/main.4da35f6d.chunk.css"
+  },
+  {
+    "revision": "0c37d6306d0126ec615d",
+    "url": "/my-tpp/static/js/2.46b6183e.chunk.js"
+  },
+  {
+    "revision": "865b23f103a429672f40",
+    "url": "/my-tpp/static/js/main.dc3adaf9.chunk.js"
+  },
+  {
+    "revision": "0d915b725ed7dd5372d2",
+    "url": "/my-tpp/static/js/runtime~main.646873ca.js"
+  },
+  {
+    "revision": "57e4ebeb0ce802d1be62b9ddfc812376",
+    "url": "/my-tpp/static/media/arrow-r.57e4ebeb.svg"
+  },
+  {
+    "revision": "4ea269b1de8507c812252ea1acf0fdd6",
+    "url": "/my-tpp/static/media/back.4ea269b1.svg"
+  },
+  {
+    "revision": "f973a2f11cbc5dce08614bf08f39d0b5",
+    "url": "/my-tpp/static/media/message.f973a2f1.svg"
+  },
+  {
+    "revision": "3220df66cd21d234fd016392f0b357e9",
+    "url": "/my-tpp/static/media/scan.3220df66.svg"
+  },
+  {
+    "revision": "bb0b8b5a507c2d2db722382d5081a909",
+    "url": "/my-tpp/static/media/search.bb0b8b5a.svg"
+  },
+  {
+    "revision": "82051390e31c4c14d89beaf8946f58d8",
+    "url": "/my-tpp/static/media/setting.82051390.svg"
+  },
+  {
+    "revision": "6fdeeb03ecb14be56e20bbfbb151b806",
+    "url": "/my-tpp/static/media/share.6fdeeb03.svg"
+  },
+  {
+    "revision": "46d07c394dd7c35fa96ba8fbaaf4a620",
+    "url": "/my-tpp/static/media/star-o.46d07c39.svg"
+  },
+  {
+    "revision": "9354869d7c3a7e3882805f1c1cc58a43",
+    "url": "/my-tpp/static/media/star.9354869d.svg"
+  },
+  {
+    "revision": "e76a3e77cbe5d3ef5b44c974f7e252d0",
+    "url": "/my-tpp/static/media/zan-o.e76a3e77.svg"
+  },
+  {
+    "revision": "f993be5368644a078033dcb313c0091e",
+    "url": "/my-tpp/static/media/zan-solid.f993be53.svg"
+  }
+]);
